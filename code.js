1699f38ca6991534c9c139af5c0fa472c92fe3a8
ross223 <144744308+ross223@users.noreply.github.com>
2024-04-21 19:59:58 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,31 @@
+const numCheck = async (num, key) => {
+    return new Promise((resolve) => {
+        resolve(num == key);
+    });
+}
 
+async function checkAsync(arr, key){
+    const results = await Promise.all(arr.map(async (num) => { // This site https://www.30secondsofcode.org/js/s/async-array-loops/ and 
+        const match = await numCheck(num, key);                // this site https://medium.com/sliit-foss/js-async-await-in-array-methods-9142a35c6d6f
+        if (match == true) {                                   // helped me understand promise functions
+            return 1;
+        } else {
+            return 0;
+        }
+    }));
+    const total = await results.reduce((tmp, current) => tmp + current, 0);
+    console.log(total);
+    return total;
+}
+
+async function doThing(arr, key) {
+    const results = await checkAsync(arr, key);
+    console.log(results);
+    return results;
+}
+
+var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var key1 = 1;
+doThing(arr1, key1);
+
+module.exports = {numCheck, checkAsync, doThing};
